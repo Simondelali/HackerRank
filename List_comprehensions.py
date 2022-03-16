fb012e735f7ab86dc605450df17ea8b77b3dffa4
Simon Atiegar <83236698+Simondelali@users.noreply.github.com>
2022-03-16 02:58:08 +0000

    Update List_comprehensions.py

--- List_comprehensions.py
+++ List_comprehensions.py
@@ -7,6 +7,6 @@ output=[]
 for i in range(x+1):
     for j in range(y+1):
         for k in range(z+1):
-            if i+j+k !=n:
+            if i+j+k !=n: 
                 output.append([i,j,k])
 print(output)
