9e6ce9252963c2ef2d3b63aa72ed3a60f3aafd85
Simon Atiegar <83236698+Simondelali@users.noreply.github.com>
2022-03-16 02:58:21 +0000

    Update List_comprehensions.py

--- List_comprehensions.py
+++ List_comprehensions.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 output=[]
-for i in range(x+1):
+for i in range(x+1):  
     for j in range(y+1):
         for k in range(z+1):
             if i+j+k !=n: 
