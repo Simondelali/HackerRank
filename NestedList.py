b0a6b82d1a545c6c3c94a44c4b210c79c5abb06b
Simon Atiegar <atiegardelali@gmail.com>
2022-03-18 00:11:42 +0000

    more commit

--- NestedList.py
+++ NestedList.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
         records.append([name, score])
     second_highest = sorted(set([score for name, score in records]))[1]
     print('\n'.join(sorted([name for name, score in records if score == second_highest])))
-        
+          
