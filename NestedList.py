a2bec1e1a2081f892be3d88fbeab1f3fdc7d294d
Simon Atiegar <83236698+Simondelali@users.noreply.github.com>
2022-03-25 01:26:27 +0000

    Update NestedList.py

--- NestedList.py
+++ NestedList.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
         score = float(input())
         records.append([name, score])
     second_highest = sorted(set([score for name, score in records]))[1] 
-    print('\n'.join(sorted([name for name, score in records if score == second_highest])))
+    print('\n'.join(sorted([name for name, score in records if score == second_highest])))  
             
