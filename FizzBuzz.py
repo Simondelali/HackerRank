03b17e20a1935ef8693a1c73e5d6367ba38e21da
Dela <83236698+Simondelali@users.noreply.github.com>
2022-09-30 18:58:05 +0000

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+def fizzBuzz(n):
+    for i in range(1,n+1):
+        if (i%3)==0 and (i%5)==0:
+            print("FizzBuzz")
+        elif (i%3)==0 and (i%5)!=0:
+            print("Fizz")
+        elif (i%5)==0 and (i%3)!=0:
+            print("Buzz")
+        else:
+            print(i)
+
+while True:
+    n = int(input("Enter a number to play :"))   
+    fizzBuzz(n)
